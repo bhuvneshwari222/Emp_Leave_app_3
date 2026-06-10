9150b384ce49a96374ca0818bbbc87ddf5e3d358
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-06-11 00:08:23 +0530

    Task_3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Emp_Leave_app_3';
+  @ViewChild('empForm') empForm!: NgForm;
+
+  onSubmit() {
+    if (this.empForm.invalid) {
+      return this.empForm.control.markAllAsTouched()
+    }
+    this.empForm.resetForm()
+  }
 }
